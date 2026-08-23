dbdc37868805600793f2e55dc4e8ed7c7fc2d7b3
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-08-23 19:37:06 +0300

    create tabs

--- components/TitleGame.tsx
+++ components/TitleGame.tsx
@@ -1,20 +1,21 @@
 import { Text, StyleSheet } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 interface TitleGameProps {
-    title: string;
+  title: string;
 }
 
 export function TitleGame({ title }: TitleGameProps) {
-    return <Text style={styles.title}>{title}</Text>;
+  return <Text style={styles.title}>{title}</Text>;
 }
 
 const styles = StyleSheet.create({
-    title: {
-        color: "#2c3e50",
-        marginBottom: 20,
-        fontSize: 24,
-        textTransform: "uppercase",
-        letterSpacing: 2,
-        fontWeight: "bold",
-    }
-})
+  title: {
+    color: "#2c3e50",
+    marginBottom: 20,
+    fontSize: 24,
+    textTransform: "uppercase",
+    letterSpacing: 2,
+    fontWeight: "bold",
+  },
+});
